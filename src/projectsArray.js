5a067e0aaeae3ec501a9d27d857bc261d3cc4706
Carl Orre <35249653+carlorre@users.noreply.github.com>
2020-10-21 09:47:30 +0200

    Update projectsArray.js

--- src/projectsArray.js
+++ src/projectsArray.js
@@ -1,19 +1,19 @@
 export const projects = [
   {
-    name: 'Custom Memory Game',
-    description: 'Fun little memory game created in React which allows users to chose the theme of the game. This project demonstrates the ability to use an external API to modify content dynamically. The game fetches images from the Unsplash API based on user input.',
-    technologyUsed: 'Tools used: React(hooks), Express, Node-Fetch, HTML, CSS, JavaScript',
-    gitHubLink: 'https://github.com/carlorre/react-memory-game',
-    liveDemo: 'http://localhost:3001',
-    media: './media/memory_gif.gif',
-  },
-  {
     name: 'Todo list',
     description: 'Simple todo list created with React. This project demonstrates the ability to use React classes aswell as local storage. Although the project is relatively simple, special attention was given to the structure of the project as well as accessibility. The app can easily be operated using only the keyboard. ',
     technologyUsed: 'React(classes), Local Storage, HTML, CSS, JavaScript',
     gitHubLink: 'https://github.com/carlorre/react-todo',
-    liveDemo: 'http://localhost:3002',
+    liveDemo: 'https://trusting-shannon-fe83a8.netlify.app',
     media: './media/todo_gif.gif',
+  },
+  {
+    name: 'Custom Memory Game',
+    description: 'Fun little memory game created in React which allows users to chose the theme of the game. This project demonstrates the ability to use an external API to modify content dynamically. The game fetches images from the Unsplash API based on user input.',
+    technologyUsed: 'Tools used: React(hooks), Express, Node-Fetch, HTML, CSS, JavaScript',
+    gitHubLink: 'https://github.com/carlorre/react-memory-game',
+    liveDemo: 'https://elastic-northcutt-7bf533.netlify.app',
+    media: './media/memory_gif.gif',
   },
   {
     name: 'Trash Tagger',
